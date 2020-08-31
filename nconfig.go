0c21b22368f00d7ace9e98711272289dd732ac9c
shinshin86 <beagles1986@gmail.com>
2020-08-31 17:57:11 +0900

    Fix with go-lint

--- nconfig.go
+++ nconfig.go
@@ -17,27 +17,27 @@ type Config struct {
 // New is returns the config loaded with the specified configuration name
 func New(filename string) *Config {
 	var configmap map[string]interface{}
-	DEFAULT_DIR := "./config"
-	DEFAULT_CONFIG := "default"
+	DefaultDir := "./config"
+	DefaultConfig := "default"
 
 	v := viper.New()
-	v.AddConfigPath(DEFAULT_DIR)
-	v.SetConfigName(DEFAULT_CONFIG)
+	v.AddConfigPath(DefaultDir)
+	v.SetConfigName(DefaultConfig)
 
 	err := v.ReadInConfig()
 	if err != nil {
-		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+		panic(fmt.Errorf("Fatal error config file: %s", err))
 	}
 	configmap = v.AllSettings()
 
-	if filename != DEFAULT_CONFIG {
+	if filename != DefaultConfig {
 		v2 := viper.New()
-		v2.AddConfigPath(DEFAULT_DIR)
+		v2.AddConfigPath(DefaultDir)
 		v2.SetConfigName(filename)
 
 		err2 := v2.ReadInConfig()
 		if err2 != nil {
-			panic(fmt.Errorf("Fatal error config file: %s \n", err2))
+			panic(fmt.Errorf("Fatal error config file: %s", err2))
 		}
 		configmap = merge(v, v2)
 	}
@@ -76,7 +76,7 @@ func (config *Config) Get(key string) string {
 func merge(v1, v2 *viper.Viper) map[string]interface{} {
 	v := v1.AllSettings()
 	if err := mergo.Merge(&v, v2.AllSettings(), mergo.WithOverride); err != nil {
-		panic(fmt.Errorf("Fatal error config file merge: %s \n", err))
+		panic(fmt.Errorf("Fatal error config file merge: %s", err))
 	}
 	return v
 }
