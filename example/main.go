23f851928264abecf537bf016077215e0b222242
shinshin86 <beagles1986@gmail.com>
2020-10-12 07:34:22 +0900

    Fix example

--- example/main.go
+++ example/main.go
@@ -9,10 +9,8 @@ import (
 func main() {
 	config := nconfig.New("production")
 
-	fmt.Println("=== Read config sample ===")
 	fmt.Println("hostname : ", config.Get("hostname"))
-	fmt.Println("dbname : ", config.Get("database.user"))
-	fmt.Println("dep5 value : ", config.Get("dep1.dep2.dep3.dep4.dep5.val"))
-
-	fmt.Println("Not supported read of six level : ", config.Get("dep1.dep2.dep3.dep4.dep5.dep6.val"))
+	fmt.Println("database.protocol : ", config.Get("database.protocol"))
+	fmt.Println("database.dbname : ", config.Get("database.dbname"))
+	fmt.Println("dep6 value : ", config.Get("dep1.dep2.dep3.dep4.dep5.dep6.val"))
 }
